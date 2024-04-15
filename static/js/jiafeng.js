a0eb8c3652d989b47e3301aae2a44675173da1b1
Koo <299070+Mutsuki@users.noreply.github.com>
2024-04-15 14:04:10 +0800

    Update jiafeng.js

--- static/js/jiafeng.js
+++ static/js/jiafeng.js
@@ -41,3 +41,37 @@ document.addEventListener('DOMContentLoaded', function () {
         // Possibly fall back to a more compatible method here
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyImages = document.querySelectorAll('img[data-src]');
+  
+  // 遍历所有带有data-src属性的图片
+  lazyImages.forEach(function(img) {
+    // 当图片出现在视窗内时，将data-src的值赋给src
+    if (isInViewport(img)) {
+      img.setAttribute('src', img.getAttribute('data-src'));
+      img.removeAttribute('data-src');
+    }
+  });
+  
+  // 判断元素是否在视窗内的函数
+  function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  // 监听滚动事件，动态加载图片
+  window.addEventListener('scroll', function() {
+    lazyImages.forEach(function(img) {
+      if (isInViewport(img)) {
+        img.setAttribute('src', img.getAttribute('data-src'));
+        img.removeAttribute('data-src');
+      }
+    });
+  });
+});

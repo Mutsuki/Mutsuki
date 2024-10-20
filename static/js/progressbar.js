c52420ed6865e71441b2b7dc5b6d89119a91f347
Koo <299070+Mutsuki@users.noreply.github.com>
2024-10-21 01:54:43 +0800

    update

--- static/js/progressbar.js
+++ static/js/progressbar.js
@@ -19,52 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // 初始化懒加载图片
-    function initLazyLoading() {
-        var lazyImages = document.querySelectorAll('img[data-src]');
-
-        if ('IntersectionObserver' in window) {
-            let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-                entries.forEach(function(entry) {
-                    if (entry.isIntersecting) {
-                        let img = entry.target;
-                        img.setAttribute('src', img.getAttribute('data-src'));
-                        img.removeAttribute('data-src');
-                        lazyImageObserver.unobserve(img);
-                    }
-                });
-            });
-
-            lazyImages.forEach(function(img) {
-                lazyImageObserver.observe(img);
-            });
-        } else {
-            // 回退到滚动事件监听的懒加载方法（不推荐）
-            function isInViewport(element) {
-                var rect = element.getBoundingClientRect();
-                return (
-                    rect.top >= 0 &&
-                    rect.left >= 0 &&
-                    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-                );
-            }
-
-            function lazyLoad() {
-                lazyImages.forEach(function (img) {
-                    if (isInViewport(img)) {
-                        img.setAttribute('src', img.getAttribute('data-src'));
-                        img.removeAttribute('data-src');
-                    }
-                });
-            }
-
-            lazyLoad();
-            window.addEventListener('scroll', lazyLoad);
-        }
-    }
-
     // 执行初始化函数
     initProgressBar();
-    initLazyLoading();
+
 });
